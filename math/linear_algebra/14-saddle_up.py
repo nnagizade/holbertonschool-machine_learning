208bfd1af0703d20537bef67e273b70a4abf1a36
nnagizade <nagizadenigar05@gmail.com>
2026-04-24 21:12:43 +0000

    task 14

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Defines a function that performs matrix multiplication"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Performs matrix multiplication on two numpy.ndarrays"""
+    return np.matmul(mat1, mat2)
