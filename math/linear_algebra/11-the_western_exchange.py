c7404816872af102962f7772b3024bf1873cdd9b
nnagizade <nagizadenigar05@gmail.com>
2026-04-24 21:08:00 +0000

    task 11

---
+++ math/linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Defines a function that transposes a numpy.ndarray"""
+
+
+def np_transpose(matrix):
+    """Transposes a numpy.ndarray and returns a new numpy.ndarray"""
+    return matrix.transpose()
