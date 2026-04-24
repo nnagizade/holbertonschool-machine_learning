5ea0cd7f6e7f167a043f73fb566d174544b2608f
nnagizade <nagizadenigar05@gmail.com>
2026-04-24 20:53:45 +0000

    task 6

---
+++ math/linear_algebra/6-howdy_partner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Defines a function that concatenates two arrays"""
+
+
+def cat_arrays(arr1, arr2):
+    """Concatenates two arrays and returns a new list"""
+    return arr1 + arr2
