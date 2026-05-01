8ec02faafc70278cce5b2e3631626e3755598128
nnagizade <nagizadenigar05@gmail.com>
2026-05-01 18:59:20 +0000

    task 5

--- math/advanced_linear_algebra/5-definiteness.py
+++ math/advanced_linear_algebra/5-definiteness.py
@@ -1,2 +1,33 @@
-           
-~                                                                                             
+#!/usr/bin/env python3
+"""
+Definiteness calculation module
+"""
+import numpy as np
+
+
+def definiteness(matrix):
+    """
+    Calculates the definiteness of a matrix
+    """
+    if not isinstance(matrix, np.ndarray):
+        raise TypeError("matrix must be a numpy.ndarray")
+    if len(matrix.shape) != 2 or matrix.shape[0] != matrix.shape[1]:
+        return None
+    # Check for symmetry
+    if not np.allclose(matrix, matrix.T):
+        return None
+
+    try:
+        vals = np.linalg.eigvals(matrix)
+    except Exception:
+        return None
+
+    if np.all(vals > 0):
+        return "Positive definite"
+    if np.all(vals >= 0):
+        return "Positive semi-definite"
+    if np.all(vals < 0):
+        return "Negative definite"
+    if np.all(vals <= 0):
+        return "Negative semi-definite"
+    return "Indefinite"
