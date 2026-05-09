7b9f2ce9243cb5fd1f8f769845d0fd275b4b4138
nnagizade <nagizadenigar05@gmail.com>
2026-05-09 12:11:39 +0000

    task 0 update task 1

--- math/probability/poisson.py
+++ math/probability/poisson.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Contains the Poisson class which represents a Poisson distribution
+Contains the Poisson class with PMF method
 """
 
 
@@ -11,10 +11,7 @@ class Poisson:
 
     def __init__(self, data=None, lambtha=1.0):
         """
-        Class constructor
-        Args:
-            data: a list of the data to be used to estimate the distribution
-            lambtha: the expected number of occurrences in a given time frame
+        Initialize Poisson distribution
         """
         if data is None:
             if lambtha <= 0:
@@ -25,6 +22,33 @@ class Poisson:
                 raise TypeError("data must be a list")
             if len(data) < 2:
                 raise ValueError("data must contain multiple values")
-            
-            # Lambtha of a Poisson distribution is the mean of the data
             self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """
+        Calculates the value of the PMF for a given number of 'successes'
+        Args:
+            k: the number of successes
+        Returns:
+            The PMF value for k
+        """
+        # Convert k to integer as requested
+        k = int(k)
+
+        # Poisson distribution is only defined for k >= 0
+        if k < 0:
+            return 0
+
+        # Mathematical constants
+        e = 2.7182818285
+        lambtha = self.lambtha
+
+        # Calculate factorial of k
+        factorial = 1
+        for i in range(1, k + 1):
+            factorial *= i
+
+        # PMF Formula: (e^-lambda * lambda^k) / k!
+        pmf_val = (e ** (-lambtha) * (lambtha ** k)) / factorial
+
+        return pmf_val
