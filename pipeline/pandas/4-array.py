8a0d6e0c2be24e5f47fb90e25d961901f4434c42
nnagizade <nagizadenigar05@gmail.com>
2026-05-23 19:52:28 +0000

    task 4

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -2,20 +2,11 @@
 """
 Task 4: Convert specific columns and rows to a numpy.ndarray
 """
-import pandas as pd
 
 
 def array(df):
     """
     Selects the last 10 rows of the High and Close columns
     and converts them into a numpy.ndarray.
-
-    Args:
-        df (pd.DataFrame): The dataframe to process
-
-    Returns:
-        numpy.ndarray: The selected data as an array
     """
-    # Select only High and Close, then take the last 10 rows
-    # Finally, convert to a numpy array
     return df[['High', 'Close']].tail(10).to_numpy()
