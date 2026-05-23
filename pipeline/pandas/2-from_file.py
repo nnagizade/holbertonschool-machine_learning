ec53fe4900cb78541e489981ac380c9ab7d4a167
nnagizade <nagizadenigar05@gmail.com>
2026-05-23 19:44:00 +0000

    task 2

--- pipeline/pandas/2-from_file.py
+++ pipeline/pandas/2-from_file.py
@@ -1,22 +1,19 @@
 #!/usr/bin/env python3
 """
-Module containing the function from_numpy
+Task 2: Load data from a file
 """
 import pandas as pd
 
 
-def from_numpy(array):
+def from_file(filename, delimiter):
     """
-    Creates a pd.DataFrame from a np.ndarray with alphabetical column labels
+    Loads data from a file as a pd.DataFrame
+
+    Args:
+        filename (str): the path to the file to load
+        delimiter (str): the column separator
+
+    Returns:
+        pd.DataFrame: the loaded dataframe
     """
-    # Get the number of columns in the ndarray
-    cols = array.shape[1]
-
-    # Generate a list of uppercase letters for the column names
-    # chr(65) is 'A', chr(66) is 'B', etc.
-    column_names = [chr(i) for i in range(65, 65 + cols)]
-
-    # Create the DataFrame using the array and the generated names
-    df = pd.DataFrame(array, columns=column_names)
-
-    return df
+    return pd.read_csv(filename, sep=delimiter)
