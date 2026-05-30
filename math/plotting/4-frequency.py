11c242cf381083eed6872b5a0b34d99afe968f9d
nnagizade <nagizadenigar05@gmail.com>
2026-05-30 15:41:55 +0000

    task 4 fixed

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -8,12 +8,15 @@ def frequency():
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
 
-    bins = np.arange(0, 101, 10)
+    bins = list(range(0, 110, 10))
     plt.hist(student_grades, bins=bins, edgecolor='black')
 
     plt.xlabel('Grades')
     plt.ylabel('Number of Students')
     plt.title('Project A')
-
+    plt.xticks(bins)
     plt.xlim(0, 100)
+    plt.ylim(0, 30)
+
+    plt.tight_layout()
     plt.show()
