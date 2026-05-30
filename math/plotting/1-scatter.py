10cc5a7a06e663808c4f3d84005a1693ecf5097b
nnagizade <nagizadenigar05@gmail.com>
2026-05-30 15:24:29 +0000

    task 1

--- math/plotting/1-scatter.py
+++ math/plotting/1-scatter.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+"""Module to plot a scatter plot of men's height vs weight."""
 import numpy as np
 import matplotlib.pyplot as plt
 
-def scatter():
 
+def scatter():
+    """Plots a scatter graph of randomly generated heights and weights."""
     mean = [69, 0]
     cov = [[15, 8], [8, 15]]
     np.random.seed(5)
