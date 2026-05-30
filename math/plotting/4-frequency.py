24f9297a2a38d57d316b3184d04cfc739796dbfa
nnagizade <nagizadenigar05@gmail.com>
2026-05-30 15:47:17 +0000

    task 4

--- math/plotting/4-frequency.py
+++ math/plotting/4-frequency.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 
 
 def frequency():
+    """plots a histogram of student scores for a project"""
     np.random.seed(5)
     student_grades = np.random.normal(68, 15, 50)
     plt.figure(figsize=(6.4, 4.8))
@@ -15,5 +16,6 @@ def frequency():
     plt.ylabel('Number of Students')
     plt.title('Project A')
     plt.xticks(range(0, 110, 10))
+    plt.xlim(0, 100)
 
     plt.show()
