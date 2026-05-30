1eff6f0ccf32e33630f96f6fefc6abb2291e5249
nnagizade <nagizadenigar05@gmail.com>
2026-05-30 15:58:19 +0000

    task 3 fixed

--- math/plotting/3-two.py
+++ math/plotting/3-two.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-"""Module to plot two radioactive decay curves on the same graph."""
 import numpy as np
 import matplotlib.pyplot as plt
 
 
 def two():
-    """Plots exponential decay curves for C-14 and Ra-266 with a legend."""
+    """plots x -> y1 and x -> y2 as line graphs"""
     x = np.arange(0, 21000, 1000)
     r = np.log(0.5)
     t1 = 5730
@@ -14,12 +13,14 @@ def two():
     y2 = np.exp((r / t2) * x)
     plt.figure(figsize=(6.4, 4.8))
 
-    plt.plot(x, y1, 'r:', label='C-14')
-    plt.plot(x, y2, 'g-', label='Ra-266')
+    plt.plot(x, y1, 'r--', label='C-14')
+    plt.plot(x, y2, 'g-', label='Ra-226')
+
     plt.xlabel('Time (years)')
     plt.ylabel('Fraction Remaining')
     plt.title('Exponential Decay of Radioactive Elements')
     plt.xlim(0, 20000)
     plt.ylim(0, 1)
     plt.legend(loc='upper right')
+
     plt.show()
