3e1f9f772d8cdf501b99fea6325a390d399651d7
nnagizade <nagizadenigar05@gmail.com>
2026-06-27 10:47:04 +0000

    task 0

--- supervised_learning/error_analysis/0-create_confusion.py
+++ supervised_learning/error_analysis/0-create_confusion.py
@@ -1,25 +1,8 @@
 #!/usr/bin/env python3
-"""
-Module to create a confusion matrix using numpy
-"""
+"""Creates a confusion matrix"""
 import numpy as np
 
 
 def create_confusion_matrix(labels, logits):
-    """
-    Creates a confusion matrix.
-
-    Args:
-        labels (numpy.ndarray): a one-hot encoded numpy.ndarray of shape
-                                (m, classes) containing the correct labels.
-        logits (numpy.ndarray): a one-hot encoded numpy.ndarray of shape
-                                (m, classes) containing the predicted labels.
-
-    Returns:
-        numpy.ndarray: a confusion matrix of shape (classes, classes) with row
-                       indices representing the correct labels and column
-                       indices representing the predicted labels.
-    """
-    # The dot product of labels.T (classes, m) and logits (m, classes)
-    # automatically counts the joint occurrences of true class i and pred class j
+    """Creates a confusion matrix"""
     return np.dot(labels.T, logits)
