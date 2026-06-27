814eb302ec682d258a573cdd760084cb6d504c96
nnagizade <nagizadenigar05@gmail.com>
2026-06-27 11:54:26 +0000

    task 2 fixed

--- supervised_learning/decision_tree/2-build_decision_tree.py
+++ supervised_learning/decision_tree/2-build_decision_tree.py
@@ -10,6 +10,7 @@ class Node:
 
     def __init__(self, feature=None, threshold=None, left_child=None,
                  right_child=None, is_root=False, depth=0):
+        """Initialize Node"""
         self.feature = feature
         self.threshold = threshold
         self.left_child = left_child
@@ -21,26 +22,28 @@ class Node:
     def left_child_add_prefix(self, text):
         """Add prefix for left child"""
         lines = text.split("\n")
-        new_text = "    +---> " + lines[0] + "\n"
+        new_text = "+---> " + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += ("    |     " + x) + "\n"
+            new_text += ("| " + x) + "\n"
         return new_text
 
     def right_child_add_prefix(self, text):
         """Add prefix for right child"""
         lines = text.split("\n")
-        new_text = "    +---> " + lines[0] + "\n"
+        new_text = "+---> " + lines[0] + "\n"
         for x in lines[1:]:
-            new_text += ("          " + x) + "\n"
+            new_text += ("  " + x) + "\n"
         return new_text
 
     def __str__(self):
+        """Return string representation of Node"""
         if self.is_root:
             title = "root"
         else:
             title = "node"
         node_str = (
-            f"{title} [feature={self.feature}, threshold={self.threshold}]\n"
+            f"{title} [feature={self.feature},"
+            f" threshold={self.threshold}]\n"
         )
         left_str = self.left_child_add_prefix(self.left_child.__str__())
         right_str = self.right_child_add_prefix(self.right_child.__str__())
@@ -51,12 +54,14 @@ class Leaf:
     """Leaf class for Decision Tree"""
 
     def __init__(self, value, depth=None):
+        """Initialize Leaf"""
         self.value = value
         self.depth = depth
         self.is_leaf = True
 
     def __str__(self):
-        return (f"-> leaf [value={self.value}]")
+        """Return string representation of Leaf"""
+        return f"leaf [value={self.value}]"
 
 
 class Decision_Tree:
@@ -64,6 +69,7 @@ class Decision_Tree:
 
     def __init__(self, max_depth=10, min_pop=1, seed=0,
                  split_criterion="random", root=None):
+        """Initialize Decision Tree"""
         self.rng = np.random.default_rng(seed)
         if root:
             self.root = root
@@ -77,4 +83,5 @@ class Decision_Tree:
         self.predict = None
 
     def __str__(self):
+        """Return string representation of Decision Tree"""
         return self.root.__str__()
