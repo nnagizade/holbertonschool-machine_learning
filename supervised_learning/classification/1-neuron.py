9ce97283c5a1823563da4edec5e712e210393846
nnagizade <nagizadenigar05@gmail.com>
2026-07-11 13:28:42 +0000

    task 1

---
+++ supervised_learning/classification/1-neuron.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+This module defines a single neuron performing binary classification
+with privatized attributes.
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Represents a single neuron performing binary classification.
+    """
+    def __init__(self, nx):
+        """
+        Initializes the neuron with private attributes.
+
+        Args:
+            nx (int): The number of input features to the neuron.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output."""
+        return self.__A
