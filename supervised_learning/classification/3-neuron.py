8fe587d7e772f0f056746f9072d89cc3db9bf411
nnagizade <nagizadenigar05@gmail.com>
2026-07-11 13:34:33 +0000

    task 3

---
+++ supervised_learning/classification/3-neuron.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+"""
+This module defines a single neuron performing binary classification
+with a cost computation feature.
+"""
+import numpy as np
+
+
+class Neuron:
+    """
+    Represents a single neuron performing binary classification.
+    """
+    def __init__(self, nx):
+        """
+        Initializes the neuron with private attributes.
+
+        Args:
+            nx (int): The number of input features to the neuron.
+
+        Raises:
+            TypeError: If nx is not an integer.
+            ValueError: If nx is less than 1.
+        """
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """Getter for the weights vector."""
+        return self.__W
+
+    @property
+    def b(self):
+        """Getter for the bias."""
+        return self.__b
+
+    @property
+    def A(self):
+        """Getter for the activated output."""
+        return self.__A
+
+    def forward_prop(self, X):
+        """
+        Calculates the forward propagation of the neuron.
+
+        Args:
+            X (numpy.ndarray): The input data of shape (nx, m).
+
+        Returns:
+            numpy.ndarray: The private attribute __A (the activated output).
+        """
+        Z = np.dot(self.__W, X) + self.__b
+        self.__A = 1 / (1 + np.exp(-Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        """
+        Calculates the cost of the model using logistic regression.
+
+        Args:
+            Y (numpy.ndarray): Correct labels for input data, shape (1, m).
+            A (numpy.ndarray): Activated outputs of the neuron, shape (1, m).
+
+        Returns:
+            float: The logistic regression cost.
+        """
+        m = Y.shape[1]
+        loss = -(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A))
+        cost = np.sum(loss) / m
+        return cost
