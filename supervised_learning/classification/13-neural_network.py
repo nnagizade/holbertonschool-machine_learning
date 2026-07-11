9e108ab4b33c620b6ab6f2fb31a8cc7abeaab77f
nnagizade <nagizadenigar05@gmail.com>
2026-07-11 14:01:58 +0000

    task 13

---
+++ supervised_learning/classification/13-neural_network.py
@@ -0,0 +1,138 @@
+#!/usr/bin/env python3
+"""Defines a neural network with one hidden layer performing binary
+classification"""
+import numpy as np
+
+
+class NeuralNetwork:
+    """Class that defines a neural network with one hidden layer
+    performing binary classification"""
+
+    def __init__(self, nx, nodes):
+        """Class constructor
+
+        Args:
+            nx (int): number of input features
+            nodes (int): number of nodes found in the hidden layer
+        """
+        if type(nx) is not int:
+            raise TypeError("nx must be an integer")
+        if nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if type(nodes) is not int:
+            raise TypeError("nodes must be an integer")
+        if nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.zeros((nodes, 1))
+        self.__A1 = 0
+
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """Getter for the weights vector of the hidden layer"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """Getter for the bias of the hidden layer"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """Getter for the activated output of the hidden layer"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """Getter for the weights vector of the output neuron"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """Getter for the bias of the output neuron"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """Getter for the activated output of the output neuron"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network
+
+        Args:
+            X (numpy.ndarray): shape (nx, m) that contains the input data
+
+        Returns:
+            The private attributes __A1 and __A2, respectively
+        """
+        Z1 = np.matmul(self.__W1, X) + self.__b1
+        self.__A1 = 1 / (1 + np.exp(-Z1))
+
+        Z2 = np.matmul(self.__W2, self.__A1) + self.__b2
+        self.__A2 = 1 / (1 + np.exp(-Z2))
+
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """Calculates the cost of the model using logistic regression
+
+        Args:
+            Y (numpy.ndarray): shape (1, m) that contains the correct labels
+            A (numpy.ndarray): shape (1, m) containing the activated output
+                of the neuron for each example
+
+        Returns:
+            The cost
+        """
+        m = Y.shape[1]
+        cost = -(1 / m) * np.sum(
+            Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)
+        )
+        return cost
+
+    def evaluate(self, X, Y):
+        """Evaluates the neural network's predictions
+
+        Args:
+            X (numpy.ndarray): shape (nx, m) that contains the input data
+            Y (numpy.ndarray): shape (1, m) that contains the correct labels
+
+        Returns:
+            The neuron's prediction and the cost of the network,
+            respectively
+        """
+        _, A2 = self.forward_prop(X)
+        cost = self.cost(Y, A2)
+        prediction = np.where(A2 >= 0.5, 1, 0)
+        return prediction, cost
+
+    def gradient_descent(self, X, Y, A1, A2, alpha=0.05):
+        """Calculates one pass of gradient descent on the neural network
+
+        Args:
+            X (numpy.ndarray): shape (nx, m) that contains the input data
+            Y (numpy.ndarray): shape (1, m) that contains the correct labels
+            A1 (numpy.ndarray): the output of the hidden layer
+            A2 (numpy.ndarray): the predicted output
+            alpha (float): the learning rate
+        """
+        m = Y.shape[1]
+
+        dZ2 = A2 - Y
+        dW2 = (1 / m) * np.matmul(dZ2, A1.T)
+        db2 = (1 / m) * np.sum(dZ2, axis=1, keepdims=True)
+
+        dZ1 = np.matmul(self.__W2.T, dZ2) * (A1 * (1 - A1))
+        dW1 = (1 / m) * np.matmul(dZ1, X.T)
+        db1 = (1 / m) * np.sum(dZ1, axis=1, keepdims=True)
+
+        self.__W1 = self.__W1 - alpha * dW1
+        self.__b1 = self.__b1 - alpha * db1
+        self.__W2 = self.__W2 - alpha * dW2
+        self.__b2 = self.__b2 - alpha * db2
