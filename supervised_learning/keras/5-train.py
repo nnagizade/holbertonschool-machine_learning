7c7c26a5779f066126a0a0391c6a51eda8181270
nnagizade <nagizadenigar05@gmail.com>
2026-07-18 06:38:37 +0000

    task 5:

---
+++ supervised_learning/keras/5-train.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+"""Trains a model using mini-batch gradient descent."""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                 validation_data=None, verbose=True, shuffle=False):
+    """Trains a model using mini-batch gradient descent.
+
+    Args:
+        network: the model to train
+        data (numpy.ndarray): shape (m, nx) containing the input data
+        labels (numpy.ndarray): one-hot shape (m, classes) with the
+            labels of data
+        batch_size: the size of the batch used for mini-batch
+            gradient descent
+        epochs: the number of passes through data for mini-batch
+            gradient descent
+        validation_data: the data to validate the model with, if not
+            None
+        verbose (bool): determines if output should be printed during
+            training
+        shuffle (bool): determines whether to shuffle the batches
+            every epoch. Normally, it is a good idea to shuffle, but
+            for reproducibility, we have chosen to set the default to
+            False
+
+    Returns:
+        the History object generated after training the model
+    """
+    history = network.fit(data, labels,
+                           batch_size=batch_size,
+                           epochs=epochs,
+                           validation_data=validation_data,
+                           verbose=verbose,
+                           shuffle=shuffle)
+
+    return history
