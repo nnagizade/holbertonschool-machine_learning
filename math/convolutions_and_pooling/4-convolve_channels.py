09d95ce280e928ecc05f7a842764be1014fa75c4
nnagizade <nagizadenigar05@gmail.com>
2026-08-08 13:03:51 +0000

    task 4

---
+++ math/convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Convolution with Channels module."""
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """Performs a convolution on images with channels.
+
+    Args:
+        images: numpy.ndarray of shape (m, h, w, c) containing multiple
+            images
+        kernel: numpy.ndarray of shape (kh, kw, c) containing the kernel
+            for the convolution
+        padding: either a tuple of (ph, pw), 'same', or 'valid'
+        stride: tuple of (sh, sw)
+
+    Returns:
+        A numpy.ndarray containing the convolved images
+    """
+    m, h, w, c = images.shape
+    kh, kw, kc = kernel.shape
+    sh, sw = stride
+
+    if padding == 'same':
+        ph = ((h - 1) * sh + kh - h) // 2 + 1
+        pw = ((w - 1) * sw + kw - w) // 2 + 1
+    elif padding == 'valid':
+        ph, pw = 0, 0
+    else:
+        ph, pw = padding
+
+    padded = np.pad(images,
+                     ((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                     mode='constant')
+
+    output_h = (h + 2 * ph - kh) // sh + 1
+    output_w = (w + 2 * pw - kw) // sw + 1
+
+    output = np.zeros((m, output_h, output_w))
+
+    for i in range(output_h):
+        for j in range(output_w):
+            y = i * sh
+            x = j * sw
+            output[:, i, j] = np.sum(
+                padded[:, y:y + kh, x:x + kw, :] * kernel,
+                axis=(1, 2, 3)
+            )
+
+    return output
