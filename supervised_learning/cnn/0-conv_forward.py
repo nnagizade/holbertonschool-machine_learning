e8c56284c4ff0d6080d7187e0586534d056902f1
nnagizade <nagizadenigar05@gmail.com>
2026-08-08 13:20:45 +0000

    task 0

--- supervised_learning/cnn/0-conv_forward.py
+++ supervised_learning/cnn/0-conv_forward.py
@@ -26,8 +26,10 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
     sh, sw = stride
 
     if padding == 'same':
-        ph = ((h_prev - 1) * sh + kh - h_prev) // 2 + 1
-        pw = ((w_prev - 1) * sw + kw - w_prev) // 2 + 1
+        ph = int(np.ceil(((h_prev - 1) * sh + kh - h_prev) / 2))
+        pw = int(np.ceil(((w_prev - 1) * sw + kw - w_prev) / 2))
+        ph = max(ph, 0)
+        pw = max(pw, 0)
     else:
         ph, pw = 0, 0
 
