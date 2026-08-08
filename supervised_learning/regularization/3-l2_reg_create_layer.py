9312b606b277b747b5a02e0b68548f20f56345bc
nnagizade <nagizadenigar05@gmail.com>
2026-08-08 12:38:17 +0000

    task 3

--- supervised_learning/regularization/3-l2_reg_create_layer.py
+++ supervised_learning/regularization/3-l2_reg_create_layer.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
-"""Module to create a layer with L2 regularization in TensorFlow."""
+"""Create a Layer with L2 Regularization module."""
 import tensorflow as tf
 
 
 def l2_reg_create_layer(prev, n, activation, lambtha):
-    """Creates a neural network layer in TensorFlow including L2 regularization.
+    """Creates a neural network layer in tensorFlow that includes
+    L2 regularization.
 
     Args:
-        prev: tensor containing the output of the previous layer
-        n: number of nodes the new layer should contain
-        activation: activation function that should be used on the layer
-        lambtha: L2 regularization parameter
+        prev: a tensor containing the output of the previous layer
+        n: the number of nodes the new layer should contain
+        activation: the activation function that should be used
+        lambtha: the L2 regularization parameter
 
     Returns:
         The output of the new layer
     """
-    kernel_initializer = tf.keras.initializers.VarianceScaling(
-        scale=2.0, mode='fan_avg'
-    )
-    layer = tf.keras.layers.Dense(
-        units=n,
-        activation=activation,
-        kernel_initializer=kernel_initializer,
-        kernel_regularizer=tf.keras.regularizers.L2(lambtha)
-    )
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode='fan_avg')
+    reg = tf.keras.regularizers.L2(lambtha)
+
+    layer = tf.keras.layers.Dense(units=n,
+                                   activation=activation,
+                                   kernel_initializer=init,
+                                   kernel_regularizer=reg)
+
     return layer(prev)
