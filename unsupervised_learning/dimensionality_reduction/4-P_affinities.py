f7df47a7ffb9fd393a7c653b1232ff64f1a9b046
nnagizade <nagizadenigar05@gmail.com>
2026-08-22 18:42:56 +0000

    task 4

---
+++ unsupervised_learning/dimensionality_reduction/4-P_affinities.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+"""Defines a function that calculates the symmetric P affinities."""
+import numpy as np
+P_init = __import__('2-P_init').P_init
+HP = __import__('3-entropy').HP
+
+
+def P_affinities(X, tol=1e-5, perplexity=30.0):
+    """
+    Calculate the symmetric P affinities of a data set.
+
+    X: a numpy.ndarray of shape (n, d) containing the dataset to be
+        transformed by t-SNE
+        n is the number of data points
+        d is the number of dimensions in each point
+    perplexity: the perplexity that all Gaussian distributions should
+        have
+    tol: the maximum tolerance allowed (inclusive) for the difference
+        in Shannon entropy from perplexity for all Gaussian
+        distributions
+
+    Returns: P, a numpy.ndarray of shape (n, n) containing the
+        symmetric P affinities
+    """
+    n, d = X.shape
+
+    D, P, betas, H = P_init(X, perplexity)
+
+    for i in range(n):
+        low = None
+        high = None
+        beta = betas[i]
+
+        Di = np.delete(D[i], i)
+
+        Hi, Pi = HP(Di, beta)
+        H_diff = Hi - H
+
+        while np.abs(H_diff) > tol:
+            if H_diff > 0:
+                low = beta[0]
+                if high is None:
+                    beta[0] = beta[0] * 2
+                else:
+                    beta[0] = (beta[0] + high) / 2
+            else:
+                high = beta[0]
+                if low is None:
+                    beta[0] = beta[0] / 2
+                else:
+                    beta[0] = (beta[0] + low) / 2
+
+            Hi, Pi = HP(Di, beta)
+            H_diff = Hi - H
+
+        P[i, np.arange(n) != i] = Pi
+        betas[i] = beta
+
+    P = (P + P.T) / (2 * n)
+
+    return P
