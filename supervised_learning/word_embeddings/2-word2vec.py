af5204add6d901ed0edeea350054b7fcf3d3cf05
nnagizade <nagizadenigar05@gmail.com>
2026-08-27 19:18:04 +0000

    task 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,34 +1,26 @@
 #!/usr/bin/env python3
-"""Module to train Word2Vec model using gensim."""
+"""LSA module using Gensim."""
 import gensim
 
 
-def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Creates, builds, and trains a gensim word2vec model."""
-    sg = 0 if cbow else 1
+def lsa(sentences, num_topics):
+    """Calculates LSA on a given corpus of sentences."""
+    # 1. Create the dictionary from tokenized sentences
+    dictionary = gensim.corpora.Dictionary(sentences)
 
-    if gensim.__version__[0] < '4':
-        return gensim.models.Word2Vec(
-            sentences=sentences,
-            size=vector_size,
-            min_count=min_count,
-            window=window,
-            negative=negative,
-            sg=sg,
-            iter=epochs,
-            seed=seed,
-            workers=workers
-        )
+    # 2. Build Bag-of-Words corpus
+    corpus = [dictionary.doc2bow(text) for text in sentences]
 
-    return gensim.models.Word2Vec(
-        sentences=sentences,
-        vector_size=vector_size,
-        min_count=min_count,
-        window=window,
-        negative=negative,
-        sg=sg,
-        epochs=epochs,
-        seed=seed,
-        workers=workers
+    # 3. Apply TF-IDF model transformation
+    tfidf = gensim.models.TfidfModel(corpus)
+    corpus_tfidf = tfidf[corpus]
+
+    # 4. Train LsiModel with the TF-IDF corpus and dictionary
+    lsi_model = gensim.models.LsiModel(
+        corpus_tfidf,
+        id2word=dictionary,
+        num_topics=num_topics
     )
+
+    # 5. Return the projection matrix U
+    return lsi_model.projection.u
