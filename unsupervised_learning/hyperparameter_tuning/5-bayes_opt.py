81e354b871c507a2dcb8ba48f643a2e48d30788f
nnagizade <nagizadenigar05@gmail.com>
2026-08-27 15:17:17 +0000

    task 5 fixed

--- unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
+++ unsupervised_learning/hyperparameter_tuning/5-bayes_opt.py
@@ -61,8 +61,9 @@ class BayesianOptimization:
             mask = sigma != 0
             Z[mask] = imp[mask] / sigma[mask]
             EI = np.zeros_like(imp)
-            EI[mask] = (imp[mask] * norm.cdf(Z[mask]) +
-                        sigma[mask] * norm.pdf(Z[mask]))
+            term1 = imp[mask] * norm.cdf(Z[mask])
+            term2 = sigma[mask] * norm.pdf(Z[mask])
+            EI[mask] = term1 + term2
         X_next = self.X_s[np.argmax(EI)]
         return X_next, EI
 
