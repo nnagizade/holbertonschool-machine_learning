1225f60e17903066b023d7a83c4263267b642210
nnagizade <nagizadenigar05@gmail.com>
2026-08-27 19:07:15 +0000

    task 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,41 +1,20 @@
-#!/usr/bin/env python3
-"""
-Module to train a Word2Vec model using gensim.
-"""
-import gensim
+from gensim.models import Word2Vec
 
-
-def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """
-    Creates, builds, and trains a gensim word2vec model.
-
-    Parameters:
-    - sentences (list): List of sentences to be trained on.
-    - vector_size (int): Dimensionality of the embedding layer.
-    - min_count (int): Minimum occurrence threshold for training.
-    - window (int): Maximum distance between current and predicted word.
-    - negative (int): Size of negative sampling.
-    - cbow (bool): True for CBOW, False for Skip-gram.
-    - epochs (int): Number of iterations to train over.
-    - seed (int): Seed for the random number generator.
-    - workers (int): Number of worker threads to train the model.
-
-    Returns:
-    - Trained Word2Vec model.
-    """
-    sg = 0 if cbow else 1
-
-    model = gensim.models.Word2Vec(
+def train_word2vec(sentences, vector_size=100, min_count=5, window=5, 
+                   negative=5, cbow_mean=True, training_algorithm=0, 
+                   learn_rate=0.025, epochs=30, seed=0, workers=1):
+    
+    model = Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
         window=window,
         negative=negative,
-        sg=sg,
+        cbow_mean=int(cbow_mean),
+        sg=training_algorithm,
+        alpha=learn_rate,
         epochs=epochs,
-        seed=seed,
+        seed=seed,         # Ensure seed parameter is dynamically passed
         workers=workers
     )
-
     return model
