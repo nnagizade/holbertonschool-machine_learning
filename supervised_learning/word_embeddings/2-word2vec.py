e6637ba72c7292527d80bf528bfc456af52a09af
nnagizade <nagizadenigar05@gmail.com>
2026-08-27 19:04:35 +0000

    task 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -2,7 +2,7 @@
 """
 Module to train a Word2Vec model using gensim.
 """
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
@@ -26,7 +26,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
     """
     sg = 0 if cbow else 1
 
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
