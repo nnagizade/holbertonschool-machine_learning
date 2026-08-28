64243941213286865817ec9e3e04cbecf3df1ce9
nnagizade <nagizadenigar05@gmail.com>
2026-08-28 10:58:19 +0000

    task 3

--- supervised_learning/word_embeddings/3-gensim_to_keras.py
+++ supervised_learning/word_embeddings/3-gensim_to_keras.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 """Converts a gensim word2vec model to a keras Embedding layer"""
+import tensorflow as tf
 
 
 def gensim_to_keras(model):
@@ -11,4 +12,14 @@ def gensim_to_keras(model):
     Returns:
         the trainable keras Embedding layer
     """
-    return model.wv.get_keras_embedding(train_embeddings=True)
+    vectors = model.wv.vectors
+    vocab_size, embedding_dim = vectors.shape
+
+    embedding_layer = tf.keras.layers.Embedding(
+        input_dim=vocab_size,
+        output_dim=embedding_dim,
+        weights=[vectors],
+        trainable=True
+    )
+
+    return embedding_layer
