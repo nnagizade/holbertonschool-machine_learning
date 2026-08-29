dcea006cc460487071b91785bab68997f37179ec
nnagizade <nagizadenigar05@gmail.com>
2026-08-29 09:28:40 +0000

    task 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,31 +1,32 @@
 #!/usr/bin/env python3
-"""Trains a gensim word2vec model"""
-import gensim
+"""Creates, builds, and trains a gensim word2vec model"""
+from gensim.models import Word2Vec
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
                     negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Creates, builds, and trains a gensim word2vec model.
+    """Creates, builds, and trains a gensim word2vec model
 
     Args:
-        sentences: list of sentences to be trained on
-        vector_size: dimensionality of the embedding layer
-        min_count: minimum number of occurrences of a word for use
-                   in training
-        window: maximum distance between the current and predicted
-                word within a sentence
-        negative: size of negative sampling
-        cbow: boolean, True is for CBOW, False is for Skip-gram
-        epochs: number of iterations to train over
-        seed: seed for the random number generator
-        workers: number of worker threads to train the model
+        sentences: a list of sentences to be trained on
+        vector_size: the dimensionality of the embedding layer
+        min_count: the minimum number of occurrences of a word for use
+            in training
+        window: the maximum distance between the current and predicted
+            word within a sentence
+        negative: the size of negative sampling
+        cbow: a boolean to determine the training type; True is for
+            CBOW; False is for Skip-gram
+        epochs: the number of iterations to train over
+        seed: the seed for the random number generator
+        workers: the number of worker threads to train the model
 
     Returns:
         the trained model
     """
     sg = 0 if cbow else 1
 
-    model = gensim.models.Word2Vec(
+    model = Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
@@ -34,7 +35,6 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         sg=sg,
         epochs=epochs,
         seed=seed,
-        workers=workers
-    )
+        workers=workers)
 
     return model
