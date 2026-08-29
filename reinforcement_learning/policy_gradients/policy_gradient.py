01e13aef743e8aa091a189f2389928eb953719ba
nnagizade <nagizadenigar05@gmail.com>
2026-08-29 16:27:12 +0000

    Task 1: Monte-Carlo policy gradient

--- reinforcement_learning/policy_gradients/policy_gradient.py
+++ reinforcement_learning/policy_gradients/policy_gradient.py
@@ -17,3 +17,26 @@ def policy(matrix, weight):
     z = matrix.dot(weight)
     exp = np.exp(z - np.max(z))
     return exp / exp.sum(axis=1, keepdims=True)
+
+
+def policy_gradient(state, weight):
+    """
+    Compute the Monte-Carlo policy gradient based on a state and weight matrix.
+
+    Args:
+        state: matrix representing the current observation of the environment
+        weight: matrix of random weight
+
+    Returns:
+        the action and the gradient (in this order)
+    """
+    P = policy(state, weight)
+    action = np.random.choice(len(P[0]), p=P[0])
+
+    s = P.reshape(-1, 1)
+    softmax_grad = np.diagflat(s) - np.dot(s, s.T)
+    dsoftmax = softmax_grad[action, :]
+    dlog = dsoftmax / P[0, action]
+    gradient = state.T.dot(dlog[None, :])
+
+    return action, gradient
