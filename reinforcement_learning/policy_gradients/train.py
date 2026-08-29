0407559a385c91fc83445871e9bd255ccc29b5bc
nnagizade <nagizadenigar05@gmail.com>
2026-08-29 16:32:37 +0000

    Task 2: implement training

---
+++ reinforcement_learning/policy_gradients/train.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Training module for policy gradient."""
+import numpy as np
+policy_gradient = __import__('policy_gradient').policy_gradient
+
+
+def train(env, nb_episodes, alpha=0.000045, gamma=0.98):
+    """
+    Implement a full training.
+
+    Args:
+        env: initial environment
+        nb_episodes: number of episodes used for training
+        alpha: the learning rate
+        gamma: the discount factor
+
+    Returns:
+        all values of the score (sum of all rewards during one episode loop)
+    """
+    weight = np.random.rand(4, 2)
+    scores = []
+
+    for episode in range(nb_episodes):
+        state, _ = env.reset()
+        state = state[None, :]
+        grads = []
+        rewards = []
+        score = 0
+
+        done = False
+        while not done:
+            action, grad = policy_gradient(state, weight)
+            next_state, reward, terminated, truncated, _ = env.step(action)
+            done = terminated or truncated
+
+            grads.append(grad)
+            rewards.append(reward)
+            score += reward
+
+            state = next_state[None, :]
+
+        for i in range(len(grads)):
+            future_rewards = sum(
+                [r * (gamma ** t) for t, r in enumerate(rewards[i:])]
+            )
+            weight += alpha * grads[i] * future_rewards
+
+        scores.append(score)
+        print("Episode: {} Score: {}".format(episode, score), end="\r", flush=False)
+
+    return scores
