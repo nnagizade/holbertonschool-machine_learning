cddb2058f4c991d0816d89001e2ae2105378fb51
nnagizade <nagizadenigar05@gmail.com>
2026-08-29 08:31:53 +0000

    task 2

--- supervised_learning/qa_bot/2-qa.py
+++ supervised_learning/qa_bot/2-qa.py
@@ -1,13 +1,31 @@
 #!/usr/bin/env python3
-"""Script that takes in input from the user and prints a response,
-exiting on exit/quit/goodbye/bye (case insensitive).
+"""Module that answers questions from a reference text using a loop,
+based on the question_answer function from task 0.
 """
+question_answer = __import__('0-qa').question_answer
 
 exit_words = {"exit", "quit", "goodbye", "bye"}
 
-while True:
-    question = input("Q: ")
-    if question.lower() in exit_words:
-        print("A: Goodbye")
-        break
-    print("A:")
+
+def answer_loop(reference):
+    """Answers questions from a reference text in a loop.
+
+    Args:
+        reference: the reference text.
+
+    Loops indefinitely, printing 'A: Goodbye' and exiting when the
+    user inputs exit/quit/goodbye/bye (case insensitive). If the
+    answer cannot be found in the reference text, responds with
+    'Sorry, I do not understand your question.'
+    """
+    while True:
+        question = input("Q: ")
+        if question.lower() in exit_words:
+            print("A: Goodbye")
+            break
+
+        answer = question_answer(question, reference)
+        if answer is None:
+            print("A: Sorry, I do not understand your question.")
+        else:
+            print("A: {}".format(answer))
