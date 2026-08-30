dd29b4ea05600d88b150b3b0a784204b7b9de790
nnagizade <nagizadenigar05@gmail.com>
2026-08-30 15:05:16 +0000

    task 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """Module that builds and trains a gensim Word2Vec model."""
-from gensim.models import Word2Vec
+import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
@@ -25,7 +25,7 @@ def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
         Word2Vec: the trained model.
     """
     sg = 0 if cbow else 1
-    model = Word2Vec(
+    model = gensim.models.Word2Vec(
         sentences=sentences,
         vector_size=vector_size,
         min_count=min_count,
