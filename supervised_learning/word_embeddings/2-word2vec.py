d7526146abbdd90c52e898c6404661e6bcbf7b36
nnagizade <nagizadenigar05@gmail.com>
2026-08-30 15:26:23 +0000

    task 2

--- supervised_learning/word_embeddings/2-word2vec.py
+++ supervised_learning/word_embeddings/2-word2vec.py
@@ -1,44 +1,22 @@
 #!/usr/bin/env python3
-"""Module that builds and trains a gensim Word2Vec model."""
+"""Creates, builds and trains a gensim word2vec model"""
 import gensim
 
 
 def word2vec_model(sentences, vector_size=100, min_count=5, window=5,
-                    negative=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Create, build, and train a gensim Word2Vec model.
+                   negative=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Creates, builds and trains a gensim word2vec model
 
-    Args:
-        sentences (list): list of sentences to be trained on.
-        vector_size (int): dimensionality of the embedding layer.
-        min_count (int): minimum number of occurrences of a word for
-            use in training.
-        window (int): maximum distance between the current and
-            predicted word within a sentence.
-        negative (int): size of negative sampling.
-        cbow (bool): determines the training type; True is for CBOW,
-            False is for Skip-gram.
-        epochs (int): number of iterations to train over.
-        seed (int): seed for the random number generator.
-        workers (int): number of worker threads to train the model.
-
-    Returns:
-        Word2Vec: the trained model.
+    Returns: the trained model
     """
-    sg = 0 if cbow else 1
-    model = gensim.models.Word2Vec(
-        sentences=sentences,
-        vector_size=vector_size,
-        min_count=min_count,
-        window=window,
-        negative=negative,
-        sg=sg,
-        seed=seed,
-        workers=workers,
-        epochs=epochs,
-    )
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=model.epochs,
-    )
+    model = gensim.models.Word2Vec(vector_size=vector_size,
+                                   min_count=min_count,
+                                   window=window,
+                                   negative=negative,
+                                   sg=0 if cbow else 1,
+                                   epochs=epochs,
+                                   seed=seed,
+                                   workers=workers,
+                                   sorted_vocab=0)
+    model.build_vocab(sentences)
     return model
