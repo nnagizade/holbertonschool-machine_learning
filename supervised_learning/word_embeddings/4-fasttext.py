0819a260b12a2235cb5b713d5bb4027761073f5a
nnagizade <nagizadenigar05@gmail.com>
2026-08-30 15:35:16 +0000

    task 4

--- supervised_learning/word_embeddings/4-fasttext.py
+++ supervised_learning/word_embeddings/4-fasttext.py
@@ -1,44 +1,24 @@
 #!/usr/bin/env python3
-"""Module that builds and trains a gensim FastText model."""
+"""Creates, builds and trains a gensim fastText model"""
 import gensim
 
 
 def fasttext_model(sentences, vector_size=100, min_count=5, negative=5,
-                    window=5, cbow=True, epochs=5, seed=0, workers=1):
-    """Create, build, and train a gensim FastText model.
+                   window=5, cbow=True, epochs=5, seed=0, workers=1):
+    """Creates, builds and trains a gensim fastText model
 
-    Args:
-        sentences (list): list of sentences to be trained on.
-        vector_size (int): dimensionality of the embedding layer.
-        min_count (int): minimum number of occurrences of a word for
-            use in training.
-        negative (int): size of negative sampling.
-        window (int): maximum distance between the current and
-            predicted word within a sentence.
-        cbow (bool): determines the training type; True is for CBOW,
-            False is for Skip-gram.
-        epochs (int): number of iterations to train over.
-        seed (int): seed for the random number generator.
-        workers (int): number of worker threads to train the model.
-
-    Returns:
-        FastText: the trained model.
+    Returns: the trained model
     """
-    sg = 0 if cbow else 1
-    model = gensim.models.FastText(
-        sentences=sentences,
-        vector_size=vector_size,
-        min_count=min_count,
-        window=window,
-        negative=negative,
-        sg=sg,
-        seed=seed,
-        workers=workers,
-        epochs=epochs,
-    )
-    model.train(
-        sentences,
-        total_examples=model.corpus_count,
-        epochs=model.epochs,
-    )
+    model = gensim.models.FastText(sentences=sentences,
+                                   vector_size=vector_size,
+                                   min_count=min_count,
+                                   negative=negative,
+                                   window=window,
+                                   sg=0 if cbow else 1,
+                                   epochs=epochs,
+                                   seed=seed,
+                                   workers=workers)
+    model.train(sentences,
+                total_examples=model.corpus_count,
+                epochs=model.epochs)
     return model
